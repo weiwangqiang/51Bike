2c27b2d9d62d4944af032ccd5df08c818130dee1
weiwangqiang <1198039102@qq.com>
2017-01-07 20:27:51 +0800

    gitceshi

---
+++ app/src/main/java/com/joshua/a51bike/activity/core/master.java
@@ -0,0 +1,24 @@
+package com.joshua.a51bike.activity.core;
+
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by wangqiang on 2017/1/7.
+ */
+public class master extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+
+    }
+}
